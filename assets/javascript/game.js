f08ad21b58d79ff2fd2d5530fa6a67625788c003
Ashley Waldner <ashleywaldner22@gmail.com>
2020-04-14 19:12:35 -0500

    set up firebase

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    var config = {
+        apiKey: "AIzaSyCajZLrdmZ7wqYy62g26GpyOeRPhUAdDDc",
+        authDomain: "rock-paper-scissors-f04de.firebaseapp.com",
+        databaseURL: "https://rock-paper-scissors-f04de.firebaseio.com",
+        projectId: "rock-paper-scissors-f04de",
+        storageBucket: "rock-paper-scissors-f04de.appspot.com"
+    };
+
+    firebase.intializeApp(config);
+
+    var database = firebase.database();
+
+    var chat = "";
+
+    $("#submit").on("click", function(event){
+        event.preventDefault();
+
+        chat = $("#chatText").val().trim();
+
+        database.ref().push({
+            chat: chat, 
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        $("#chatText")[0].reset();
+    })
+
+    database.ref().on("child_added", function (childSnapshot){
+        
+    })
+
+
+}); //document.ready dont delete
